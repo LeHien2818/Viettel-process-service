b7a71e601fc5280f2041fd4ca1467106dbca0a58
LeHien2818 <hienltv281823@gmail.com>
2025-06-12 02:58:02 +0700

    fix(Dockerfile): update build java version

--- app/src/main/java/call/processing/app/config/KafkaStreamConfig.java
+++ app/src/main/java/call/processing/app/config/KafkaStreamConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
@@ -13,12 +14,22 @@ import org.springframework.kafka.config.KafkaStreamsConfiguration;
 @Configuration
 @EnableKafkaStreams
 public class KafkaStreamConfig {
+    @Value("${kafka.broker.address}")
+    private String kafkaAddress;
 
     @Bean
     public KafkaStreamsConfiguration defaultKafkaStreamsConfig() {
+        String kafkaConnectionServer = "localhost:9092,localhost:9093";
+        if(kafkaAddress != null){
+            if(!kafkaAddress.equals(kafkaConnectionServer) &&
+                !kafkaAddress.equals("default")){
+                kafkaConnectionServer = kafkaAddress;
+            }
+        }
+        
         final Map<String, Object> props = new HashMap<>();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "call-app");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConnectionServer);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, GsonSerde.class);
 
